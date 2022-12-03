94369b6659537ac4124dd3f921ea16c826bba35a
bfayers <bfayers@users.noreply.github.com>
2022-12-03 08:09:04 +0000

    day 3 solution

---
+++ day3/day3.py
@@ -0,0 +1,52 @@
+import string
+
+priorities = list(string.ascii_lowercase)
+priorities.insert(0, 0)
+
+# data = """vJrwpWtwJgWrhcsFMMfFFhFp
+# jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+# PmmdzqPrVvPwwTWBwg
+# wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+# ttgJtRGJQctTZtZT
+# CrZsJsPPZsGzwwsLwLmpwMDw"""
+# data = data.split("\n")
+
+with open("input.txt") as f:
+    data = f.read().split("\n")
+data = data[:-1]
+
+total = 0
+
+# Part 1
+for line in data:
+    # Split line in half
+    first_half = set(line[0 : int(len(line) / 2)])
+    second_half = set(line[int(len(line) / 2) :])
+
+    # Find items in both compartments
+    in_both = first_half.intersection(second_half)
+    for item in in_both:
+        # Add up the priorities
+        total += priorities.index(item.lower())
+        if item.isupper():
+            total += 26
+
+print(total)
+
+# Part 2
+total = 0
+chunk_size = 3
+for i in range(0, len(data), chunk_size):
+    chunk = data[i : i + chunk_size]
+    first = set(chunk[0])
+    second = set(chunk[1])
+    third = set(chunk[2])
+
+    in_all = set.intersection(first, second, third)
+    for item in in_all:
+        # Add up the priorities
+        total += priorities.index(item.lower())
+        if item.isupper():
+            total += 26
+
+print(total)
