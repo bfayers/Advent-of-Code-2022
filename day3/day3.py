dff4aa08508febbffcce4f44b07546b47522a23f
bfayers <bfayers@users.noreply.github.com>
2022-12-03 21:53:02 +0000

    black

--- day3/day3.py
+++ day3/day3.py
@@ -20,7 +20,7 @@ total = 0
 # Part 1
 for line in data:
     # Split line in half
-    mid = int(len(line)/2)
+    mid = int(len(line) / 2)
     first_half, second_half = set(line[:mid]), set(line[mid:])
 
     # Find items in both compartments
@@ -39,7 +39,6 @@ chunk_size = 3
 for i in range(0, len(data), chunk_size):
     chunk = data[i : i + chunk_size]
     first, second, third = set(chunk[0]), set(chunk[1]), set(chunk[2])
-
 
     in_all = set.intersection(first, second, third)
     for item in in_all:
